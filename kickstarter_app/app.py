9fe203be7d44efa1bb5ec62695b4c668dde16c66
Dylan-Sivori <Dylan.Sivori@gmail.com>
2021-03-29 19:15:51 -0400

    root html and backend working
    root html and backend updated and functioning

--- kickstarter_app/app.py
+++ kickstarter_app/app.py
@@ -2,7 +2,7 @@
 from os import getenv
 from flask import Flask, render_template, request
 from .models import DB, User
-from .twitter import get_info_and_add
+# from .twitter import get_info_and_add
 
 
 def create_app():
@@ -15,10 +15,46 @@ def create_app():
 
     @app.route("/")
     def root():
+        return render_template("base.html")
 
-        return render_template("base.html",
-                               project_name=project_name,
-                               id=id,
+    # endpoint == "user_submitted"
+
+    @app.route("/user_submitted", methods=["POST"])
+    def user_submitted():
+        project_name = request.values['project_name']
+        category = request.values['category']
+        main_category = request.values['main_category']
+        currency = request.values['currency']
+        deadline = request.values['deadline']
+        goal = request.values['goal']
+        launched = request.values['launched']
+        pledged = request.values['pledged']
+        backers = request.values['backers']
+        country = request.values['country']
+        usd_pledged = request.values['usd_pledged']
+        usd_pledged_real = request.values['usd_pledged_real']
+        usd_goal_real = request.values['usd_goal_real']
+        # username_1 = request.values["username_1"]
+        # username_2 = request.values["username_2"]
+        # dtypes: user = <user twitter object>, user_tweets = <tweets list>
+        # DB_user = get_info_and_add(username_1)
+        # DB_user2 = get_info_and_add(username_2)
+        record = User(project_name=project_name,
+                    category=category,
+                    main_category=main_category,
+                   currency=currency,
+                   deadline=deadline,
+                   goal=goal,
+                   launched=launched,
+                   pledged=pledged,
+                   backers=backers,
+                   country=country,
+                   usd_pledged=usd_pledged,
+                   usd_pledged_real=usd_pledged_real,
+                   usd_goal_real=usd_goal_real)
+        DB.session.add(record)
+        DB.session.commit()
+        return render_template("user.html",
                                project_name=project_name,
                                category=category,
                                main_category=main_category,
@@ -31,19 +67,7 @@ def create_app():
                                country=country,
                                usd_pledged=usd_pledged,
                                usd_pledged_real=usd_pledged_real,
-                               usd_goal_real=usd_goal_real
-                               )
-
-    # endpoint == "user_submitted"
-
-    @app.route("/user_submitted", methods=["POST"])
-    def user_submitted():
-        username_1 = request.values["username_1"]
-        username_2 = request.values["username_2"]
-        # dtypes: user = <user twitter object>, user_tweets = <tweets list>
-        DB_user = get_info_and_add(username_1)
-        DB_user2 = get_info_and_add(username_2)
-        return render_template("user.html", username_1=username_1, username_2=username_2, tweets=DB_user.embeddings)
+                               usd_goal_real=usd_goal_real)
 
     # endpoint == "reset"
 
